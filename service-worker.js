43aa7c04e8f06685763ac97d0b946d62b37242c1
Architect-Brad <bradleynyadimo@gmail.com>
2026-06-29 11:56:34 +0300

    fix: PWA readiness - add PNG icons, manifest/apple-touch links, cache icons in SW

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,9 @@ const ASSETS = [
     '/js/code-data.js',
     '/js/word-lists.js',
     '/manifest.json',
-    '/icons/icon.svg'
+    '/icons/icon.svg',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
